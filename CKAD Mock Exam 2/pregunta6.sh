e771e54dc7326234999b89d146c611cc039f738c
Joan Delgado <joandelgado18@gmail.com>
2026-04-05 15:22:27 -0500

    Agrega preguntas 6 y 7

--- CKAD Mock Exam 2/pregunta6.sh
+++ CKAD Mock Exam 2/pregunta6.sh
@@ -1,2 +1,7 @@
 kubectl config use-context cluster3
 kubectl create -f pregunta6.yaml
+sleep 60
+kubectl set image deployment ocean-apd ocean-apd=kodekloud/webapp-color:v2
+sleep 60
+kubectl rollout history deploy ocean-apd > /opt/ocean-revision-count.txt
+kubectl rollout undo deploy ocean-apd --to-revision=1
